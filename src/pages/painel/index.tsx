0e7d5909fe69e87dc974268d8b018fe2d60f6e58
Felipe Feliciano <fehzl@live.com>
2022-02-20 23:43:10 +0000

    :sparkles: feat: create a protected panel index page

--- src/pages/painel/index.tsx
+++ src/pages/painel/index.tsx
@@ -1,7 +1,27 @@
+import { GetServerSideProps } from 'next';
+import { getSession } from 'next-auth/react';
+
 export default function Painel() {
   return (
-    <div>
+    <div className="px-14">
       <h1>Painel</h1>
     </div>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const session = await getSession(context);
+
+  if (!session) {
+    context.res.writeHead(302, {
+      Location: `/`,
+    });
+    context.res.end();
+  }
+
+  return {
+    props: {
+      session,
+    },
+  };
+};
