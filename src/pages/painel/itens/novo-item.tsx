3737cb92cfd87eb017016fb76b7f9ec6a8cedd68
Felipe Feliciano <fehzl@live.com>
2022-02-20 23:45:20 +0000

    :sparkles: feat: create NewItem page

---
+++ src/pages/painel/itens/novo-item.tsx
@@ -0,0 +1,38 @@
+import TextInput from '@/components/text-input/text-input';
+
+export default function NewItem() {
+  return (
+    <div>
+      <form>
+        <div className="">
+          <span>.detalhes</span>
+          <div className="flex flex-row">
+            <TextInput label="código" />
+            <TextInput label="apelido" />
+            <TextInput label="descrição de exibição" />
+            <TextInput label="descrição detalhada" />
+            <TextInput label="grupo" />
+            <TextInput label="imagens" />
+          </div>
+        </div>
+        <div className="">
+          <span>.logistica</span>
+          <div className="flex flex-row">
+            <TextInput label="unidade em uma caixa" />
+            <TextInput label="peso" />
+            <TextInput label="peso líquido" />
+          </div>
+        </div>
+        <div className="">
+          <span>.vendas</span>
+          <div className="flex flex-row">
+            <TextInput label="disponível para venda" />
+            <TextInput label="preço da unidade" />
+            <TextInput label="preço da caixa" />
+          </div>
+        </div>
+        <button type="submit">salvar</button>
+      </form>
+    </div>
+  );
+}
