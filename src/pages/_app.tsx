a9d3dc3803eb1a788f8469bf905ad3f3fc6ee731
Felipe Feliciano <fehzl@live.com>
2022-02-20 23:39:16 +0000

    :sparkles: feat: implements QueryProvider, Session and MainLayout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,19 @@
 import { AppProps } from 'next/app';
 import '@/styles/global.css';
+import { SessionProvider } from 'next-auth/react';
+import MainLayout from '@/layouts/main';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const queryClient = new QueryClient();
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <SessionProvider session={pageProps.session}>
+        <MainLayout>
+          <Component {...pageProps} />
+        </MainLayout>
+      </SessionProvider>
+    </QueryClientProvider>
+  );
 }
