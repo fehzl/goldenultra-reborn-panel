f9151f61c8b599ba3fed00f897b4b195fc129a36
Felipe Feliciano <fehzl@live.com>
2022-02-20 23:32:46 +0000

    :heavy_plus_sign: chore: add tailwind-scrollbar-hide

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,10 @@ module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx}',
     './src/components/**/*.{js,ts,jsx,tsx}',
+    './src/layouts/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require('tailwind-scrollbar-hide')],
 };
