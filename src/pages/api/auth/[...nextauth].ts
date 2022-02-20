b0a5da91ca4d5f5fe2e5027567200b6a738c1522
Felipe Feliciano <fehzl@live.com>
2022-02-20 23:41:13 +0000

    :sparkles: feat: remote api authentication and set user data

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import NextAuth from 'next-auth';
 import CredentialsProvider from 'next-auth/providers/credentials';
 
@@ -8,11 +9,32 @@ export default NextAuth({
       credentials: {},
 
       async authorize(credentials: any) {
-        console.log(credentials);
-        const user = { id: `123`, name: `John Doe` };
-        if (credentials?.email === `Felipe`) return user;
-        return null;
+        const response = await axios.post(
+          `http://localhost:3333/api/v1/login`,
+          credentials,
+        );
+
+        if (response.data.httpCode === 200) {
+          return { ...response.data.data.user, jwt: response.data.data.jwt };
+        } else {
+          return null;
+        }
       },
     }),
   ],
+  callbacks: {
+    signIn: async (user: any) => {
+      return user;
+    },
+    async jwt({ token, user }) {
+      if (user) {
+        token.jwt = user.jwt;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      session.jwt = token.jwt;
+      return session;
+    },
+  },
 });
