803ef3fa726e233b980790a53f5d10beedc911d6
Felipe Feliciano <fehzl@live.com>
2022-02-26 08:11:33 +0000

    :lipstick: refactor: tailwind improvements

--- src/layouts/main.tsx
+++ src/layouts/main.tsx
@@ -10,11 +10,11 @@ export default function MainLayout({ children }: MainLayoutProps) {
   const { data: session, status } = useSession();
 
   return (
-    <div className="flex flex-row min-h-screen max-h-full p-4 space-x-4 bg-gray-100 text-gray-800">
+    <div className="flex divide-x overflow-hidden max-h-screen divide-slate-300 flex-row min-h-screen bg-gray-100 text-gray-800">
       <Sidebar />
-      <main className="main space-y-4 overflow-hidden overflow-x-hidden flex flex-col flex-grow -ml-64 md:ml-0 transition-all duration-150 ease-in">
+      <main className="divide-y divide-slate-300 main overflow-hidden overflow-x-hidden flex flex-col flex-grow -ml-64 md:ml-0 transition-all duration-150 ease-in">
         {session && <Header session={session} />}
-        <div className="bg-gray-200 min-h-full p-4 rounded-lg">{children}</div>
+        <div className="p-4 overflow-y-auto">{children}</div>
       </main>
     </div>
   );
