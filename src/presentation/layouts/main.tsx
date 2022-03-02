a579989c9a781c77e1256aa511a94a666351a9ee
Felipe Feliciano <fehzl@live.com>
2022-03-02 10:19:41 +0000

    :lipstick: refactor: remove padding form main

--- src/presentation/layouts/main.tsx
+++ src/presentation/layouts/main.tsx
@@ -12,9 +12,9 @@ export default function MainLayout({ children }: MainLayoutProps) {
   return (
     <div className="flex divide-x overflow-hidden max-h-screen divide-slate-300 flex-row min-h-screen bg-gray-100 text-gray-800">
       <Sidebar />
-      <main className="divide-y divide-slate-300 main overflow-hidden overflow-x-hidden flex flex-col flex-grow -ml-64 md:ml-0 transition-all duration-150 ease-in">
+      <main className="divide-y divide-slate-300 main overflow-hidden flex flex-col flex-grow transition-all duration-150 ease-in">
         {session && <Header session={session} />}
-        <div className="p-4 overflow-y-auto">{children}</div>
+        <div className="overflow-y-auto">{children}</div>
       </main>
     </div>
   );
