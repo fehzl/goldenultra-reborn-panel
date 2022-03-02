1cfcc4e7374ee974f1dc3fbbc891d25f1a7dabef
Felipe Feliciano <fehzl@live.com>
2022-03-02 10:16:27 +0000

    :sparkles: feat: create Logo component

---
+++ src/presentation/components/logo/logo.tsx
@@ -0,0 +1,8 @@
+export function Logo() {
+  return (
+    <span className="text-3xl text-gray-800 font-bold tracking-tighter">
+      Goldenultra
+      <span className="text-green-500 text-4xl leading-none">.</span>
+    </span>
+  );
+}
