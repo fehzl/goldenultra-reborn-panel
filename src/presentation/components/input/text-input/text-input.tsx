68e9441ae23829c64c3eea5b896bd53401b9988e
Felipe Feliciano <fehzl@live.com>
2022-03-08 20:17:55 +0000

    :sparkles: feat: create a TextInput component

--- src/presentation/components/input/text-input/text-input.tsx
+++ src/presentation/components/input/text-input/text-input.tsx
@@ -1,23 +1,32 @@
-import { FieldError } from 'react-hook-form';
+import React from 'react';
 
-interface TextInputProps extends React.InputHTMLAttributes<HTMLInputElement> {
-  label: string;
-  error?: FieldError;
-}
+type InputProps = React.DetailedHTMLProps<
+  React.InputHTMLAttributes<HTMLInputElement>,
+  HTMLInputElement
+> & {
+  label?: React.ReactNode;
+  error?: string;
+};
 
-export default function TextInput({ label, error }: TextInputProps) {
-  return (
-    <div className="mb-4 flex flex-col">
-      <label htmlFor="email" className="text-sm text-gray-700">
-        {label}
-      </label>
-      <input
-        className="bg-gray-200 text-gray-800 my-2 px-3 py-1 text-sm rounded-lg h-8"
-        type="text"
-      />
-      {error && (
-        <div className="text-red-500 text-xs mt-1">{error.message}</div>
-      )}
-    </div>
-  );
-}
+export const TextInput = React.forwardRef<HTMLInputElement, InputProps>(
+  function Input({ label, error, ...rest }: InputProps, ref) {
+    return (
+      <div className="flex flex-col">
+        {label && (
+          <label htmlFor="code" className="text-sm text-gray-500">
+            {label}
+          </label>
+        )}
+        <input
+          className={`border-2 rounded-lg text-gray-800 px-2 py-1 focus:outline-none ${
+            !error ? `border-gray-200` : `border-red-200`
+          }`}
+          autoComplete="off"
+          ref={ref}
+          {...rest}
+        />
+        <small className="text-red-400">{error}</small>
+      </div>
+    );
+  },
+);
