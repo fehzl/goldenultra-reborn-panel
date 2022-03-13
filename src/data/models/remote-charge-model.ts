0a5ff110afd1b9e9064a5a9c5409c312d1739b2c
Felipe Feliciano <fehzl@live.com>
2022-03-13 20:40:54 +0000

    :sparkles: feat: create a model to charge

---
+++ src/data/models/remote-charge-model.ts
@@ -0,0 +1,8 @@
+export type RemoteChargeModel = {
+  id: string;
+  order_id: string;
+  type: string;
+  value: number;
+  created_at: Date;
+  updated_at: Date;
+};
---
+++ src/domain/models/charge-model.ts
@@ -0,0 +1,8 @@
+export type ChargeModel = {
+  id: string;
+  order_id: string;
+  type: string;
+  value: number;
+  created_at: Date;
+  updated_at: Date;
+};
