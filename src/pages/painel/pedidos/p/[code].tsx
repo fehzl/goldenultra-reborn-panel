82ee977b9f5297bafafbb92a9b96cc14954290f9
Felipe Feliciano <fehzl@live.com>
2022-03-13 20:44:19 +0000

    :recycle: refactor: simplify code

--- src/pages/painel/pedidos/p/[code].tsx
+++ src/pages/painel/pedidos/p/[code].tsx
@@ -1,34 +1,55 @@
-import { RemoteLoadOrder } from '@/data/usecases';
-import { AxiosHttpClientAdapter } from '@/infra/http/axios-http-client-adapter';
 import { useRouter } from 'next/router';
 import { BiHash, BiShoppingBag } from 'react-icons/bi';
 import { useQuery } from 'react-query';
 import { PageHeader } from '@/presentation/components/shared/page/header';
 import { PageWrapper } from '@/presentation/components/shared/page/wrapper';
+import { ReactNode, useState } from 'react';
 import {
-  OrderClientCard,
-  OrderDetailsCard,
-  OrderItemList,
-} from '@/presentation/components/order';
+  OrderChargersTab,
+  OrderDetailsTab,
+  OrderPaymentTab,
+} from '@/presentation/components/tab';
+import { makeRemoteLoadOrder } from '@/main/factories/usecases';
 
 export default function ShowOrder() {
   const router = useRouter();
   const { code: routerCode } = router.query;
 
-  const httpClient = new AxiosHttpClientAdapter();
-  const url = `http://132.226.243.30:3333/api/v1/orders/${routerCode}`;
-  const remoteLoadOrder = new RemoteLoadOrder(url, httpClient);
+  const remoteLoadOrder = makeRemoteLoadOrder();
 
   const sections = [
     { name: `Detalhes`, activeColor: `text-green-500` },
-    { name: `Pagamentos`, activeColor: `text-orange-400` },
-    { name: `Finalizados`, activeColor: `text-red-400` },
+    { name: `Encargos`, activeColor: `text-blue-400` },
+    { name: `Pagamentos`, activeColor: `text-blue-400` },
+    { name: `Separação`, activeColor: `text-blue-400` },
+    { name: `Expedição`, activeColor: `text-blue-400` },
   ];
 
-  const { data, isLoading, isFetching } = useQuery(
+  const [activeSection, setActiveSection] = useState<string>(sections[0].name);
+
+  const { data, isLoading } = useQuery(
     [`order`, routerCode],
     async () => await remoteLoadOrder.load(routerCode as string),
   );
+
+  const renderTab = (): ReactNode => {
+    if (data) {
+      switch (activeSection) {
+        case `Detalhes`:
+          return <OrderDetailsTab data={data} />;
+        case `Encargos`:
+          return <OrderChargersTab data={data} />;
+        case `Pagamentos`:
+          return <OrderPaymentTab data={data} />;
+        case `Separação`:
+          return <div>Separação</div>;
+        case `Expedição`:
+          return <div>Expedição</div>;
+        default:
+          return <div>404</div>;
+      }
+    }
+  };
 
   return (
     <>
@@ -39,16 +60,13 @@ export default function ShowOrder() {
         subtitleIcon={<BiHash />}
         subtitleText={data?.id}
       />
-      <PageWrapper loading={isLoading || isFetching} sections={sections}>
-        <div className="flex flex-col space-y-10 px-9">
-          {data && (
-            <>
-              <OrderDetailsCard order={data} />
-              <OrderClientCard client={data.client} />
-              <OrderItemList items={data.items} />
-            </>
-          )}
-        </div>
+      <PageWrapper
+        loading={isLoading}
+        sections={sections}
+        activeSection={activeSection}
+        setActiveSection={setActiveSection}
+      >
+        <div className="space-y-10 px-9">{renderTab()}</div>
       </PageWrapper>
     </>
   );
