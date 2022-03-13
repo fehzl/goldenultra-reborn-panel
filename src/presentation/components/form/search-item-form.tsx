cfe666ed88bfc43a1a59f4bd35e4c960a50933f8
Felipe Feliciano <fehzl@live.com>
2022-03-13 20:28:31 +0000

    :recycle: refactor: move to a separate component

---
+++ src/presentation/components/form/search-item-form.tsx
@@ -0,0 +1,59 @@
+import { useQuery } from 'react-query';
+import { DeviceModel, OrderItemModel } from '@/domain/models';
+import { useState } from 'react';
+import { makeRemoteLoadDeviceList } from '@/main/factories/usecases';
+import Select from '../input/select';
+
+interface Props {
+  items: any[];
+  setItems: (items: any[]) => void;
+  selectInputRef: React.RefObject<any>;
+}
+
+export function SearchItemForm({ items, setItems, selectInputRef }: Props) {
+  const remoteLoadDeviceList = makeRemoteLoadDeviceList();
+
+  const [search, setSearch] = useState(``);
+
+  const { data } = useQuery(
+    [`devices`, search],
+    async () => await remoteLoadDeviceList.loadAll({ search, limit: 5 }),
+    { keepPreviousData: true },
+  );
+
+  const alreadyExists = (code: string): boolean => {
+    return items.some((item) => item.device.code === code);
+  };
+
+  const chooseItem = (device: DeviceModel): void => {
+    if (alreadyExists(device.code)) {
+      return;
+    }
+
+    const item: Omit<
+      OrderItemModel,
+      'id' | 'discount' | 'overall' | 'amount_separated'
+    > = {
+      device,
+      amount: null,
+      price: device.un_price,
+    };
+    setItems([...items, item]);
+  };
+
+  return (
+    <>
+      {data && (
+        <Select
+          data={data}
+          label="Inserir Item"
+          search={search}
+          setSearch={setSearch}
+          ref={selectInputRef}
+          chooseItem={chooseItem}
+          optionLabel={[`code`, `exhibition_description`]}
+        />
+      )}
+    </>
+  );
+}
