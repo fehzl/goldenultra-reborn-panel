f5a62eb2f51352d4a73593e0e0e0968a465b7f7d
Felipe Feliciano <fehzl@live.com>
2022-03-13 20:46:14 +0000

    :sparkles: feat: access token as dependency

--- src/main/decorators/authorize-http-client-decorator.ts
+++ src/main/decorators/authorize-http-client-decorator.ts
@@ -1,19 +1,16 @@
 import { HttpClient, HttpRequest, HttpResponse } from '@/data/protocols/http';
 
 export class AuthorizeHttpClientDecorator implements HttpClient {
-  constructor(private readonly httpClient: HttpClient) {}
+  constructor(
+    private readonly httpClient: HttpClient,
+    private readonly accessToken: string | undefined,
+  ) {}
 
   async request(data: HttpRequest): Promise<HttpResponse> {
-    const account = {
-      id: `any_id`,
-      name: `any_name`,
-      accessToken: `MQ.k9p_nmB9ukmARPj7GZ8CVx9WedPDvKPcrZPNxNvtHydsfGpO-Jduy_zlnMyB`,
-    };
-
-    if (account.accessToken) {
+    if (this.accessToken) {
       Object.assign(data, {
         headers: Object.assign(data.headers || {}, {
-          Authorization: `Bearer ${account.accessToken}`,
+          Authorization: `Bearer ${this.accessToken}`,
         }),
       });
     }
