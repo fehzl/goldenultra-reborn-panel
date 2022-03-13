eecefff1716fc8869a698ff790cb6096258ca483
Felipe Feliciano <fehzl@live.com>
2022-03-13 20:45:13 +0000

    :fire: refactor: remove test code

--- src/pages/painel/itens/novo-item.tsx
+++ src/pages/painel/itens/novo-item.tsx
@@ -1,38 +1,3 @@
-import TextInput from '@/presentation/components/input/text-input/text-input';
-
 export default function NewItem() {
-  return (
-    <div>
-      <form>
-        <div className="">
-          <span>.detalhes</span>
-          <div className="flex flex-row">
-            <TextInput label="código" />
-            <TextInput label="apelido" />
-            <TextInput label="descrição de exibição" />
-            <TextInput label="descrição detalhada" />
-            <TextInput label="grupo" />
-            <TextInput label="imagens" />
-          </div>
-        </div>
-        <div className="">
-          <span>.logistica</span>
-          <div className="flex flex-row">
-            <TextInput label="unidade em uma caixa" />
-            <TextInput label="peso" />
-            <TextInput label="peso líquido" />
-          </div>
-        </div>
-        <div className="">
-          <span>.vendas</span>
-          <div className="flex flex-row">
-            <TextInput label="disponível para venda" />
-            <TextInput label="preço da unidade" />
-            <TextInput label="preço da caixa" />
-          </div>
-        </div>
-        <button type="submit">salvar</button>
-      </form>
-    </div>
-  );
+  return <div>oi</div>;
 }
