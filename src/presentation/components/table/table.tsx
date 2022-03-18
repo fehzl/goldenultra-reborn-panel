2e4bbb39c1a3f5fb32ced2da40beaef2b2430148
Felipe Feliciano <fehzl@live.com>
2022-03-18 02:29:03 +0000

    :lipstick: feat: add styles to table

--- src/presentation/components/table/table.tsx
+++ src/presentation/components/table/table.tsx
@@ -39,7 +39,7 @@ interface TableProps<T extends MinTableItem> {
 export default function Table<T extends MinTableItem>(props: TableProps<T>) {
   function renderRow(item: T) {
     return (
-      <tr>
+      <>
         {objectKeys(item).map((itemProperty, index) => {
           const customRenderer = props.customRenderers?.[itemProperty];
 
@@ -57,21 +57,44 @@ export default function Table<T extends MinTableItem>(props: TableProps<T>) {
             </td>
           );
         })}
-      </tr>
+      </>
     );
   }
 
   return (
-    <table>
-      <thead>
-        {objectValues(props.headers).map((headerValue, index) => {
-          if (props.hideId && index === 0) {
-            return null;
-          }
-          return <th key={index}>{headerValue}</th>;
-        })}
-      </thead>
-      <tbody>{props.items.map(renderRow)}</tbody>
-    </table>
+    <div className="border-2 rounded-lg border-dotted">
+      <table className="min-w-full divide-y-2 divide-dotted text-center">
+        <thead>
+          <tr className="h-12">
+            {objectValues(props.headers).map((headerValue, index) => {
+              if (props.hideId && index === 0) {
+                return null;
+              }
+              return (
+                <th key={index}>
+                  <div className="flex items-center justify-center">
+                    <span className="font-normal text-sm text-gray-500">
+                      {headerValue}
+                    </span>
+                  </div>
+                </th>
+              );
+            })}
+          </tr>
+        </thead>
+        <tbody className="text-sm">
+          {props.items.map((item, index) => (
+            <tr
+              key={item.id as string}
+              className={`h-12 text-gray-700 ${
+                index % 2 === 0 ? `bg-gray-100` : ``
+              }`}
+            >
+              {renderRow(item)}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 }
