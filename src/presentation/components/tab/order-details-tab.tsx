35b64931096edef344f0d307b96677bda1135479
Felipe Feliciano <fehzl@live.com>
2022-03-23 15:08:42 +0000

    :recycle: refactor: update items table to generic table

--- src/presentation/components/tab/order-details-tab.tsx
+++ src/presentation/components/tab/order-details-tab.tsx
@@ -1,17 +1,38 @@
 import { RemoteOrderModel } from '@/data/models';
+import { numberToBrazilianReal } from '@/presentation/utils/formatters';
 import { ClientCard, OrderDetailsCard } from '../card';
-import { OrderItemList } from '../list';
+import { Table } from '../table';
 
 interface Props {
   data: RemoteOrderModel;
 }
 
 export function OrderDetailsTab({ data }: Props) {
+  const rows = data.items.map((item) => ({
+    id: item.id,
+    code: item.device.code,
+    description: item.device.exhibition_description,
+    price: numberToBrazilianReal(item.price),
+    amount: item.amount,
+    overall: numberToBrazilianReal(item.overall),
+  }));
+
   return (
     <>
       <OrderDetailsCard order={data} />
       <ClientCard client={data.client} />
-      <OrderItemList items={data.items} />
+      <Table
+        items={rows}
+        headers={{
+          id: `ID`,
+          code: `Código`,
+          description: `Descrição`,
+          price: `Preço`,
+          amount: `Quantidade`,
+          overall: `Total`,
+        }}
+        hideId
+      />
     </>
   );
 }
