6a2afde33d8bf0c97dd1ff8e324a39508e6f8491
Felipe Feliciano <fehzl@live.com>
2022-03-23 15:09:32 +0000

    :sparkles: feat: create order overall tab and card

---
+++ src/presentation/components/card/order-overall-card.tsx
@@ -0,0 +1,11 @@
+export function OrderOverallCard() {
+  return (
+    <div className="flex flex-row items-center justify-between">
+      <div>Pedido Recebido</div>
+      <div>Pedido Pago</div>
+      <div>Pedido Separado</div>
+      <div>Pedido Expedido</div>
+      <div>Pedido Recebido</div>
+    </div>
+  );
+}
---
+++ src/presentation/components/tab/order-overall-tab.tsx
@@ -0,0 +1,9 @@
+import { OrderOverallCard } from '../card';
+
+export function OrderOverallTab() {
+  return (
+    <div>
+      <OrderOverallCard />
+    </div>
+  );
+}
