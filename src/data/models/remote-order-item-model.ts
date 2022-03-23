fa994aa9e6c3b5b3f002409e85c3e0578ddd64e7
Felipe Feliciano <fehzl@live.com>
2022-03-23 15:05:17 +0000

    :label: feat: add types for separated_by

--- src/data/models/remote-order-item-model.ts
+++ src/data/models/remote-order-item-model.ts
@@ -1,4 +1,5 @@
 import { RemoteDeviceModel } from './remote-device-model';
+import { RemoteEmployeeModel } from './remote-employee-model';
 
 export type RemoteOrderItemModel = {
   id: string;
@@ -7,6 +8,8 @@ export type RemoteOrderItemModel = {
   discount: number;
   overall: number;
   separated: boolean;
+  separated_by: RemoteEmployeeModel;
+  separated_at: Date;
   amount_separated: number;
   device: RemoteDeviceModel;
 };
--- src/domain/models/order-item-model.ts
+++ src/domain/models/order-item-model.ts
@@ -1,12 +1,15 @@
 import { DeviceModel } from '.';
+import { EmployeeModel } from './employee-model';
 
 export type OrderItemModel = {
   id: string;
   price: number;
-  amount: number | null;
+  amount: number;
   discount: number;
   overall: number;
   separated: boolean;
+  separated_by: EmployeeModel;
+  separated_at: Date;
   amount_separated: number;
   device: DeviceModel;
 };
